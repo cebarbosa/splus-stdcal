6ce682353389e233a474d2aad36ffe56f832c46b
cebarbosa <carlos.barbosa@usp.br>
2019-08-22 14:27:55 -0700

    Using coordinates of stars from table considering proper motions.

--- stdphot.py
+++ stdphot.py
@@ -19,9 +19,10 @@ from astropy.io import fits
 from astropy.table import Table, hstack, vstack
 from astropy.nddata.utils import Cutout2D
 from astropy.coordinates import SkyCoord
+from astropy.time import Time
 from astropy.wcs import WCS
-from astroquery.vizier import Vizier
 from tqdm import tqdm
+import sewpy
 
 import sip_tpv
 
@@ -45,7 +46,7 @@ def make_std_cutout(nights, data_dir, outdir_root, redo=False,
                       skip_existing_nights=True, cutout_size=None):
     """ Search for catalogs and get catalog information for standard stars. """
     print("Initial number of nights: {}".format(len(nights)))
-
+    stdcoords = Table.read("tables/stdcoords.fits", format="fits")
     header_keys = ["OBJECT", "FILTER", "EXPTIME", "GAIN", "TELESCOP",
                    "INSTRUME", "AIRMASS"]
     # Skip all processing related to nights already processed before
@@ -66,12 +67,8 @@ def make_std_cutout(nights, data_dir, outdir_root, redo=False,
             print("No processed images for this night. Continuing...")
             continue
         for img in tqdm(images):
-            cat = os.path.join(path, img.replace(".fits", "files"),
-                             img.replace(".fits", ".apercorcat"))
             if not os.path.exists(outdir):
                 os.mkdir(outdir)
-            if not os.path.exists(cat):
-                continue
             outcat = os.path.join(outdir, "{}_cat.fits".format(
                 img.split("_proc")[0]))
             if os.path.exists(outcat) and not redo:
@@ -82,12 +79,15 @@ def make_std_cutout(nights, data_dir, outdir_root, redo=False,
             h = fits.getheader(os.path.join(path, img))
             sip_tpv.pv_to_sip(h)
             wcs = WCS(h, relax=True)
-            obj = h["object"]
-            q = Vizier.query_object(obj, catalog="II/246")[0]
-            starcoords = SkyCoord(ra=q["RAJ2000"].mean(),
-                                  dec=q["DEJ2000"].mean(),
-                                  unit=(u.degree, u.degree))
-            x0, y0 = wcs.all_world2pix(starcoords.ra, starcoords.dec, 1)
+            obj = h["object"].strip().lower()
+            coords = stdcoords[np.where(obj==stdcoords["STAR"])[0]]
+            starcoords = SkyCoord(ra=coords["RA_ICRS"], dec=coords["DE_ICRS"],
+                                   distance=coords["distance"],
+                                  pm_ra_cosdec=coords['pmRA'],
+                                  pm_dec=coords['pmDE'],
+                                  obstime=Time(2015.5, format='decimalyear'))
+            c = starcoords.apply_space_motion(new_obstime=Time(night))
+            x0, y0 = wcs.all_world2pix(c.ra, c.dec, 1)
             # Make cutout
             imgout = os.path.join(outdir, "{}_stamp.fits".format(
                                   img.split("_proc")[0]))
@@ -106,6 +106,8 @@ def run_sextractor(data_dir, nights, coords, redo=False):
     config_dir = os.path.split(os.path.realpath(__file__))[0]
     sex_config_file = os.path.join(config_dir, "stdcal.sex")
     sex_params_file = os.path.join(config_dir, "stdcal.param")
+    header_keys = ["OBJECT", "FILTER", "EXPTIME", "GAIN", "TELESCOP",
+                   "INSTRUME", "AIRMASS", "IMAGE", "DATE"]
     with open(sex_params_file) as f:
         sexpars = [_.strip() for _ in f.readlines()]
     for i, night in enumerate(nights):
@@ -118,33 +120,35 @@ def run_sextractor(data_dir, nights, coords, redo=False):
                         nice=19)
         for stamp in tqdm(stamps):
             imgfile = os.path.join(data_dir, night, stamp)
-            sexcat = imgfile.replace("stamp", "sexcat")
+            h = fits.getheader(imgfile, hdu=2)
+            x0, y0 = h["NAXIS1"], h["NAXIS2"]
+            sexcat = imgfile.replace(".fits", ".cat")
             if os.path.exists(sexcat) and not redo:
                 continue
             cat = sew(imgfile)["table"]
-            rsep = np.sqrt((cat["X_IMAGE"] - coords[0])**2 +
-                                    (cat["Y_IMAGE"] - coords[1])**2)
+            rsep = np.sqrt((cat["X_IMAGE"] - x0)**2 + (cat["Y_IMAGE"] - y0)**2)
             idx = np.argmin(rsep)
             if rsep[idx] <= 4:
                 cat = Table(cat[idx])
+                for key in header_keys:
+                    cat[key] = h[key]
                 cat.write(sexcat, overwrite=True)
 
 def join_tables(data_dir, nights, output, redo=True):
     """ Join standard star catalogs into one table. """
-    metafields = ["DATE", "IMAGE", "STAR", "FILTER", "AIRMASS", "EXPTIME",
-                  "D2D"]
+    hfields = ["DATE", "IMAGE", "STAR", "FILTER", "AIRMASS", "EXPTIME"]
     photfields = ["FLUX_APER", "FLUXERR_APER"]
     outtable = []
     print("Loading tables...")
     for night in tqdm(nights):
         wdir = os.path.join(data_dir, night)
-        tables = [_ for _ in os.listdir(wdir) if _.endswith("_cat.fits")]
-        for table in tables:
-            phottable = table.replace("_cat", "_sexcat")
-            if not os.path.exists(os.path.join(wdir, phottable)):
+        stamps = [_ for _ in os.listdir(wdir) if _.endswith("stamp.fits")]
+        for stamp in stamps:
+            cat = stamp.replace(".fits", ".cat")
+            if not os.path.exists(os.path.join(wdir, cat)):
                 continue
             t1 = Table.read(os.path.join(wdir, table), format="fits")[
-                metafields]
+                hfields]
             t2 = Table.read(os.path.join(wdir, phottable), format="fits")[
                 photfields]
             outtable.append(hstack([t1, t2]))
@@ -155,7 +159,6 @@ def join_tables(data_dir, nights, output, redo=True):
     outtable["STAR"] = stars
     outtable.write(output, format="fits", overwrite=True)
     return output
-
 
 def main():
     config_files = [_ for _ in sys.argv if _.endswith(".yaml")]
@@ -172,7 +175,7 @@ def main():
         # Removing nights without standard stars
         nights = [night for night in nights if
                   any(s.startswith("EXTMONI") and s.endswith("_proc.fits")
-           for s in os.listdir(os.path.join(config["singles_dir"], night)))]
+               for s in os.listdir(os.path.join(config["singles_dir"], night)))]
         extmoni_dir = os.path.join(config["output_dir"], "extmoni")
         if not os.path.exists(extmoni_dir):
             os.mkdir(extmoni_dir)
