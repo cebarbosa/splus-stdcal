a43cc73fb93097c7654eb39593f6b5f3b8a81be3
cebarbosa <carlos.barbosa@usp.br>
2020-03-26 16:24:49 -0300

    Complete version of the new version of the code.

--- splus-stdcal/calib_single.py
+++ splus-stdcal/calib_single.py
@@ -17,13 +17,11 @@ import yaml
 import platform
 
 import numpy as np
-from astropy.table import Table
+from astropy.table import Table, vstack
 from astropy.io import fits
-from multiprocessing import Pool
 from tqdm import tqdm
 
 import context
-import misc
 
 def calib_single_catalogs(zptable, flux_keys, survey, redo=False):
     """ Performs the calibration of catalogs from single exposure. """
@@ -117,26 +115,143 @@ def calib_single_catalogs(zptable, flux_keys, survey, redo=False):
                 os.mkdir(output_dir)
             data.write(output, overwrite=True)
 
-def main():
-    config_files = [_ for _ in sys.argv if _.endswith(".yaml")]
-    if len(config_files) == 0:
-        default_file = "config_mode5.yaml"
-        print("Using default config file ({})".format(default_file))
-        config_files.append(default_file)
-    for filename in config_files:
-        with open(filename) as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-        # Set nights that will be calibrated
-        nights = misc.select_nights(config)
-        singles_dir = os.path.join(config["output_dir"], "single")
-        # pool = Pool(25)
-        # pool.map(f, nights)
+def load_date_zps(fname, nstars=3, noutliers=0):
+    """ Makes nested dictionary for calibration parameters.
+
+    Input parameters
+    ----------------
+    fname: str
+        Input filename FITS table.
+
+    nstars: int (optional)
+        Minimum number of stars used in the calibration of the night.
+
+    noutliers: int (optional)
+        Maximum number of outliers in the fit.
+
+    Output parameters
+    -----------------
+    dict
+        Nested dictionary containing all zero points in the table. Results are
+        accessed using dict[date][parname][filter], where parname includes
+        zp, zperr, kappa and kappaerr.
+
+    """
+    table = Table.read(fname)
+    results = {}
+    for line in table:
+        z, zerr, k, kerr = {}, {}, {}, {}
+        d = {}
+        for band in context.bands:
+            zpkey = "ZP_{}".format(band)
+            if zpkey not in table.colnames:
+                continue
+            # Select only nights without outliers
+            ntot = line["Ntot_{}".format(band)]
+            n = line["N_{}".format(band)]
+            nout = ntot - n
+            condition = (ntot >= nstars) & (nout <= noutliers)
+            if not condition:
+                continue
+            z[band] = line[zpkey]
+            zerr[band] = line["eZP_{}".format(band)]
+            k[band] = line["K_{}".format(band)]
+            kerr[band] = line["eK_{}".format(band)]
+        d["zp"] = z
+        d["zperr"] = zerr
+        d["kappa"] = k
+        d["kappaerr"] = kerr
+        results[line["DATE"]] = d
+    return results
+
+def load_header_fits_rec(catfile, hdu=1):
+    """ Load header parameters from a LDAC_FITS file (not compatible with
+    astropy) into a dictionary.
+
+    Input parameters
+    ----------------
+    catfile: str
+        Path for the LDAC_FITS file
+
+    hdu: int (optional)
+        Number of the hdu to be read.
+
+
+    Output parameters
+    -----------------
+    dict
+        Dictionary containing the header information, including values and
+        comments (Warning: always strings).
+
+    """
+    hdata = str(fits.getdata(catfile, hdu=hdu)[0])[12:-23]
+    hdata = hdata.replace(",", " ").replace("'", "").replace(
+        '"', "")
+    hdata = [_.strip() for _ in hdata.split("\n")]
+    hdict = {}
+    for line in hdata:
+        if len(line.split("=")) < 2:
+            continue
+        keywd, val = line.split("=", 1)
+        hdict[keywd.strip()] = [_.strip() for _ in val.split("/")]
+    return hdict
+
+
+def calib_no_outliers():
+    """ Calibration script for the cases without outliers. """
+    config_files = ["config_mode0.yaml", "config_mode5.yaml"]
+    for j, config_file in enumerate(config_files):
+        print("Config file {} / {}".format(j+1, len(config_files)))
+        with open(config_file, "r") as f:
+            config = yaml.load(f)
+        calib_dir = os.path.join(config["main_dir"], "calib")
+        outdir = os.path.join(config["main_dir"], "zeropoints")
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        single_dir_out = os.path.join(outdir, "single")
+        if not os.path.exists(single_dir_out):
+            os.mkdir(single_dir_out)
+        calibs = [_ for _ in os.listdir(calib_dir) if _.startswith(
+                          config["name"])]
+        latest = sorted(calibs)[-1]
+        single_dir = os.path.join(config["main_dir"], "single")
+        for phot in config["sex_phot"]:
+            print("Processing photometry mode {}".format(phot))
+            fname = os.path.join(calib_dir, latest, phot, "zps-dates.fits")
+            zpdata = load_date_zps(fname)
+            output = os.path.join(single_dir_out, "{}-{}.fits".format(
+                                  config["name"], phot))
+            outtable = []
+            for i, (date, params) in enumerate(tqdm(zpdata.items())):
+                zp = params["zp"]
+                zperr = params["zperr"]
+                kappa = params["kappa"]
+                kappaerr = params["kappaerr"]
+                data_dir = os.path.join(single_dir, date)
+                if not os.path.exists(data_dir):
+                    continue
+                singles = sorted(os.listdir(data_dir))
+                for single in singles:
+                    t = Table()
+                    catfile = os.path.join(data_dir, single)
+                    hdict = load_header_fits_rec(catfile)
+                    band = hdict["FILTER"][0]
+                    if band not in zp:
+                        continue
+                    X = float(hdict["AIRMASS"][0])
+                    exptime = float(hdict["EXPTIME"][0])
+                    m0 = zp[band] - kappa[band] * X + 2.5 * np.log10(exptime)
+                    m0err = np.sqrt(zperr[band]**2 + (kappaerr[band] * X)**2)
+                    t["OBJECT"] = [hdict["OBJECT"][0]]
+                    t["DATE"] = [hdict["DATE-OBS"][0]]
+                    t["FILTER"] = [band]
+                    t["EXPTIME"] = [exptime]
+                    t["GAIN"] = [float(hdict["GAIN"][0])]
+                    t["ZP"] = [np.round(m0, 5)]
+                    t["ZPERR"] = [np.round(m0err, 5)]
+                    outtable.append(t)
+            outtable = vstack(outtable)
+            outtable.write(output, format="fits", overwrite=True)
 
 if __name__ == "__main__":
-    main()
-
-    # flux_keys = ["FLUX_AUTO", "FLUX_ISO", "FLUXAPERCOR"]
-    # survey = "STRIPE82"
-    # redo=True
-    # table = Table.read(os.path.join(context.tables_dir, "date_zps.fits"))
-    # calib_single_catalogs(table, flux_keys, survey, redo=True)
+    calib_no_outliers()
